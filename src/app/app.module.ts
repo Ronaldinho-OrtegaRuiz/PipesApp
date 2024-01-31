037381eb08094100a6e5a69d85df8097855c04f8
Ronaldinho Ortega Ruiz <112998261+Ronaldinho-OrtegaRuiz@users.noreply.github.com>
2024-01-31 13:42:52 -0500

    Instalar PrimeNg, utilizarlo para hacer un menú, menuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {SharedModule} from "./shared/shared.module";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     provideClientHydration()
